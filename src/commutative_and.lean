6572f30f6ce35dccc266451edfe28c8a7705e66f
Ujjwal <ujjwalupadhyay8@gmail.com>
2021-01-04 02:25:59 +0530

    commutative and

--- src/commutative_and.lean
+++ src/commutative_and.lean
@@ -1,5 +1,8 @@
+--stating theorem p and q are proposition
 theorem and_commutative (p q : Prop) : p ∧ q → q ∧ p :=
+--assumption hypothesis on pq
 assume hpq : p ∧ q,
 have hp : p, from and.left hpq,
 have hq : q, from and.right hpq,
+--show
 show q ∧ p, from and.intro hq hp
